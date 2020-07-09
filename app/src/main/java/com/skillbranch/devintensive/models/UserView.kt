725f737231f402445dc792a8789fb1630e067c77
Denis <denisv39@mail.ru>
2020-07-09 22:00:16 +0200

    hometask_2 is done

---
+++ app/src/main/java/com/skillbranch/devintensive/models/UserView.kt
@@ -0,0 +1,10 @@
+package com.skillbranch.devintensive.models
+
+class UserView(
+    val id: String,
+    val fullName:String,
+    val nickName:String,
+    var avatar: String? = null,
+    var status: String = "offline",
+    val initials: String?
+)
